f518a4626903b19bc963df8b53fa40c11b2f9860
joacomonsalvo <jmonsalvo.business@protonmail.com>
2021-10-30 19:42:54 -0300

    first commit

---
+++ pixel.py
@@ -0,0 +1,65 @@
+import numpy as np
+import cv2.cv2
+import tkinter as tk
+from sklearn.cluster import KMeans
+from tkinter import filedialog
+
+
+ROOT = tk.Tk().withdraw()
+IMG_PATH = cv2.cv2.imread(filedialog.askopenfilename())
+
+
+def color_clustering(idx, image, k):
+    cluster_values = []
+    for _ in range(0, k):
+        cluster_values.append([])
+    
+    for r in range(0, idx.shape[0]):
+        for c in range(0, idx.shape[1]):
+            cluster_values[idx[r][c]].append(image[r][c])
+
+    img_c = np.copy(image)
+
+    cluster_averages = []
+    for i in range(0, k):
+        cluster_averages.append(np.average(cluster_values[i], axis=0))
+
+    for r in range(0, idx.shape[0]):
+        for c in range(0, idx.shape[1]):
+            img_c[r][c] = cluster_averages[idx[r][c]]
+            
+    return img_c
+
+
+def segment_img_clr_rgb(image, k):
+    
+    img_c = np.copy(IMG_PATH)
+    
+    h = image.shape[0]
+    w = image.shape[1]
+    
+    img_c.shape = (image.shape[0] * image.shape[1], 3)
+    kmeans = KMeans(n_clusters=k, random_state=0).fit_predict(img_c)
+    kmeans.shape = (h, w)
+
+    return kmeans
+
+
+def k_means_image(image, k):
+    idx = segment_img_clr_rgb(image, k)
+    return color_clustering(idx, image, k)
+
+
+def pixelate(image, w, h):
+    height, width = image.shape[:2]
+    temp = cv2.cv2.resize(IMG_PATH, (w, h), interpolation=cv2.cv2.INTER_LINEAR)
+
+    return cv2.cv2.resize(temp, (width, height), interpolation=cv2.cv2.INTER_NEAREST)
+
+
+if __name__ == '__main__':
+    #  Show image
+    IMG_PATH = pixelate(IMG_PATH, 64, 64)
+    IMG_PATH = k_means_image(IMG_PATH, 5)
+    cv2.cv2.imshow('image', IMG_PATH)
+    cv2.cv2.waitKey(0)
